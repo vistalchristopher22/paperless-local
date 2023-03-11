f5a799fe0abbd26845a6b2925584fc08ce25eb54
CHRis <christophervistal25@gmail.com>
2023-03-11 09:24:59 +0800

    Add seeder for sp members & add package for settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\SanggunianMember;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            SanggunianMemberSeeder::class,
         ]);
     }
 }
