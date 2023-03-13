d74d6c50ba9bd1b7da775678cf3cac16e16b66e8
CHRis <christophervistal25@gmail.com>
2023-03-13 09:27:28 +0800

    Add method docblock

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -13,6 +13,12 @@ class DivisionController extends Controller
     {
     }
 
+    /**
+     * It returns a view called `admin.division.index` with a variable called `division` that contains
+     * the result of the `get()` function in the `divisionRepository` class
+     * 
+     * @return The view admin.division.index and the division variable is being passed to the view.
+     */
     public function index()
     {
         return view('admin.division.index', [
@@ -25,6 +31,11 @@ class DivisionController extends Controller
         return view('admin.division.create');
     }
 
+    /**
+     * It stores a new division in the database
+     * 
+     * @param DivisionStoreRequest request The request object.
+     */
     public function store(DivisionStoreRequest $request)
     {
         $this->divisionRepository->store($request->all());
@@ -44,6 +55,14 @@ class DivisionController extends Controller
         ]);
     }
 
+    /**
+     * It updates a division
+     * 
+     * @param DivisionUpdateRequest request The request object.
+     * @param Division division The model that we are updating.
+     * 
+     * @return A view with a success message.
+     */
     public function update(DivisionUpdateRequest $request, Division $division)
     {
         $this->divisionRepository->update($division, $request->all());
@@ -51,10 +70,17 @@ class DivisionController extends Controller
         return back()->with('success', 'You have successfully updated a division.');
     }
 
+    /**
+     * The function takes a division object as a parameter, and then deletes the division object from
+     * the database
+     * 
+     * @param Division division This is the model that we are using.
+     * 
+     * @return A view with a success message.
+     */
     public function destroy(Division $division)
     {
         $this->divisionRepository->delete($division);
-
         return back()->with('success', 'You have successfully deleted a division.');
     }
 }
