90490bf0cb1f11c246ebd2bf7a4df53895ca3d3c
CHRis <christophervistal25@gmail.com>
2023-03-13 10:24:35 +0800

    Display profile picture

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -288,8 +288,8 @@
                         <button class="btn btn-link p-0 position-relative" type="button" id="profileDropdown"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             <picture>
-                                <img class="f-w-10 rounded-circle" src="/assets/images/profile-small.jpeg"
-                                    alt="HTML Bootstrap Admin Template by Pixel Rocket">
+                                <img class="f-w-10 rounded-circle" src="{{ asset('/storage/user-images/' . auth()->user()->profile_picture) }}"
+                                    alt="Profile Picture">
                             </picture>
                             <span
                                 class="position-absolute bottom-0 start-75 p-1 bg-success border border-3 border-white rounded-circle">
@@ -298,13 +298,13 @@
                         </button>
                         <ul class="dropdown-menu dropdown-md dropdown-menu-end" aria-labelledby="profileDropdown">
                             <li class="d-flex py-2 align-items-start">
-                                <button class="btn-icon bg-primary-faded text-primary fw-bolder me-3">J</button>
+                                <button class="btn-icon bg-primary-faded text-primary fw-bolder me-3">{{ Str::substr(auth()->user()->first_name, 0, 1) }}</button>
                                 <div class="d-flex align-items-start justify-content-between flex-grow-1">
                                     <div>
                                         <p class="lh-1 mb-2 fw-semibold text-body text-capitalize">
                                             {{ auth()->user()->last_name }},
                                             {{ auth()->user()->first_name }}</p>
-                                        <p class="text-muted lh-1 mb-2 small">{{ '@' . auth()->user()->username }}</p>
+                                        <p class="text-muted lh-1 mb-2 small">{{ auth()->user()->account_type }}</p>
                                     </div>
                                 </div>
                             </li>
